241c784f48250b050fa05e4b0ddb00e70945eddb
krishnaji020210 <krishnaji020210@gmail.com>
2026-03-25 14:50:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VISCITY/sreta.git",
+    "https://github.com/krishnaji020210/Sreta.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

8f45a06c215a901be1fb4ab09b39bff028c8bf5a
yourfather4901-gif <yourfather4901@gmail.com>
2026-03-26 15:28:00 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -1,102 +1,129 @@
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ButtonStyle
 
 from AnonXMusic import app
 
 
 def help_pannel(_, is_sudo, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSE_BUTTON"],
+            callback_data="close",
+            style=ButtonStyle.DANGER
+        )
+    ]
+
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data="settingsback_helper",
+            style=ButtonStyle.DEFAULT
         ),
     ]
+
     mark = second if START else first
+
     upl = [
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
-            ]
+        [
+            InlineKeyboardButton(
+                text=_["H_B_1"],
+                callback_data="help_callback hb1",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_2"],
+                callback_data="help_callback hb2",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_3"],
+                callback_data="help_callback hb3",
+                style=ButtonStyle.PRIMARY
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_4"],
+                callback_data="help_callback hb4",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_5"],
+                callback_data="help_callback hb5",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_6"],
+                callback_data="help_callback hb6",
+                style=ButtonStyle.PRIMARY
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_7"],
+                callback_data="help_callback hb7",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_8"],
+                callback_data="help_callback hb8",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_9"],
+                callback_data="help_callback hb9",
+                style=ButtonStyle.PRIMARY
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_10"],
+                callback_data="help_callback hb10",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_11"],
+                callback_data="help_callback hb11",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_12"],
+                callback_data="help_callback hb12",
+                style=ButtonStyle.PRIMARY
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["H_B_13"],
+                callback_data="help_callback hb13",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_14"],
+                callback_data="help_callback hb14",
+                style=ButtonStyle.PRIMARY
+            ),
+            InlineKeyboardButton(
+                text=_["H_B_15"],
+                callback_data="help_callback hb15",
+                style=ButtonStyle.PRIMARY
+            ),
         ]
-    
+    ]
+
     if is_sudo:
         upl.append(
             [
                 InlineKeyboardButton(
                     text="Ai/TTS/IMAGE Settings",
                     callback_data="help_callback hb16",
+                    style=ButtonStyle.SUCCESS
                 )
             ]
         )
-    
+
     upl.append(mark)
 
     return InlineKeyboardMarkup(upl)
@@ -108,7 +135,8 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
+                    callback_data="settings_back_helper",
+                    style=ButtonStyle.DEFAULT
                 ),
             ]
         ]
@@ -122,6 +150,7 @@ def private_help_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 url=f"https://t.me/{app.username}?start=help",
+                style=ButtonStyle.PRIMARY
             ),
         ],
     ]
